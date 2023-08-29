583a8d93dacde7a1fb7dd3d713d4967364a385a6
Sebastiá <97sebastiangb@gmail.com>
2023-08-29 10:48:08 +0200

    Change environment

--- src/app/add.ts
+++ src/app/add.ts
@@ -1,53 +1,82 @@
-import { randomUUID } from 'crypto';
-import { getType } from 'mime';
-import { program } from 'commander';
-import * as p from '@clack/prompts';
-import { existsSync, readFileSync } from 'fs-extra';
-import { checkLatest } from '../api/update';
-import { checkAppExistsAndHasPermissionErr, checkAppExistsAndHasPermission, newIconPath, Options } from '../api/app';
+import { randomUUID } from "crypto";
+import { getType } from "mime";
+import { program } from "commander";
+import * as p from "@clack/prompts";
+import { existsSync, readFileSync } from "fs-extra";
+import { checkLatest } from "../api/update";
 import {
-  getConfig, createSupabaseClient,
-  findSavedKey, useLogSnag, verifyUser, formatError
-} from '../utils';
+  checkAppExistsAndHasPermissionErr,
+  checkAppExistsAndHasPermission,
+  newIconPath,
+  Options,
+} from "../api/app";
+import {
+  getConfig,
+  createSupabaseClient,
+  findSavedKey,
+  useLogSnag,
+  verifyUser,
+  formatError,
+} from "../utils";
 
-export const addApp = async (appId: string, options: Options, shouldExit = true) => {
+export const addApp = async (
+  appId: string,
+  options: Options,
+  shouldExit = true
+) => {
   if (shouldExit) {
     p.intro(`Adding`);
   }
   await checkLatest();
-  options.apikey = options.apikey || findSavedKey()
+  options.apikey = options.apikey || findSavedKey();
   const config = await getConfig();
-  appId = appId || config?.app?.appId
-  const snag = useLogSnag()
+  appId = appId || config?.app?.appId;
+  const snag = useLogSnag();
 
   if (!options.apikey) {
-    p.log.error(`Missing API key, you need to provide a API key to upload your bundle`);
-    program.error('');
+    p.log.error(
+      `Missing API key, you need to provide a API key to upload your bundle`
+    );
+    program.error("");
   }
   if (!appId) {
-    p.log.error("Missing argument, you need to provide a appId, or be in a capacitor project");
-    program.error('');
+    p.log.error(
+      "Missing argument, you need to provide a appId, or be in a capacitor project"
+    );
+    program.error("");
   }
-  const supabase = createSupabaseClient(options.apikey)
+  const supabase = createSupabaseClient(options.apikey);
 
-  const userId = await verifyUser(supabase, options.apikey, ['write', 'all']);
+  const userId = await verifyUser(supabase, options.apikey, ["write", "all"]);
   // Check we have app access to this appId
   if (shouldExit) {
-    await checkAppExistsAndHasPermissionErr(supabase, appId, options.apikey, false);
+    await checkAppExistsAndHasPermissionErr(
+      supabase,
+      appId,
+      options.apikey,
+      false
+    );
   } else {
-    const res = await checkAppExistsAndHasPermission(supabase, appId, options.apikey, false);
+    const res = await checkAppExistsAndHasPermission(
+      supabase,
+      appId,
+      options.apikey,
+      false
+    );
     if (res) {
-      return false
+      return false;
     }
   }
 
   let { name, icon } = options;
-  appId = appId || config?.app?.appId
-  name = name || config?.app?.appName || 'Unknown'
-  icon = icon || "resources/icon.png" // default path for capacitor app
+  appId = appId || config?.app?.appId;
+  name = name || config?.app?.appName || "Unknown";
+  icon = icon || "resources/icon.png"; // default path for capacitor app
   if (!icon || !name) {
-    p.log.error("Missing argument, you need to provide a appId and a name, or be in a capacitor project");
-    program.error('');
+    p.log.error(
+      "Missing argument, you need to provide a appId and a name, or be in a capacitor project"
+    );
+    program.error("");
   }
   if (shouldExit) {
     p.log.info(`Adding ${appId} to Capgo`);
@@ -58,29 +87,31 @@ export const addApp = async (appId: string, options: Options, shouldExit = true)
   if (icon && existsSync(icon)) {
     iconBuff = readFileSync(icon);
     const contentType = getType(icon);
-    iconType = contentType || 'image/png';
+    iconType = contentType || "image/png";
     p.log.warn(`Found app icon ${icon}`);
-  }
-  else if (existsSync(newIconPath)) {
+  } else if (existsSync(newIconPath)) {
     iconBuff = readFileSync(newIconPath);
     const contentType = getType(newIconPath);
-    iconType = contentType || 'image/png';
+    iconType = contentType || "image/png";
     p.log.warn(`Found app icon ${newIconPath}`);
   } else {
-    p.log.warn(`Cannot find app icon in any of the following locations: ${icon}, ${newIconPath}`);
+    p.log.warn(
+      `Cannot find app icon in any of the following locations: ${icon}, ${newIconPath}`
+    );
   }
 
   // check if app already exist
   const { data: app, error: dbError0 } = await supabase
-    .rpc('exist_app_v2', { appid: appId })
-    .single()
+    .rpc("exist_app_v2", { appid: appId })
+    .single();
   if (app || dbError0) {
     p.log.error(`App ${appId} already exists ${formatError(dbError0)}`);
-    program.error('');
+    program.error("");
   }
 
-  const fileName = `icon_${randomUUID()}`
-  let signedURL = 'https://supabase.elcomandero.es/storage/v1/object/public/images/capgo.png'
+  const fileName = `icon_${randomUUID()}`;
+  let signedURL =
+    "https://supabase.elcomandero.es/storage/v1/object/public/images/capgo.png";
 
   // upload image if available
   if (iconBuff && iconType) {
@@ -88,65 +119,73 @@ export const addApp = async (appId: string, options: Options, shouldExit = true)
       .from(`images/${userId}/${appId}`)
       .upload(fileName, iconBuff, {
         contentType: iconType,
-      })
+      });
     if (error) {
       p.log.error(`Could not add app ${formatError(error)}`);
-      program.error('');
+      program.error("");
     }
-    const { data: signedURLData } = await supabase
-      .storage
+    const { data: signedURLData } = await supabase.storage
       .from(`images/${userId}/${appId}`)
-      .getPublicUrl(fileName)
-    signedURL = signedURLData?.publicUrl || signedURL
+      .getPublicUrl(fileName);
+    signedURL = signedURLData?.publicUrl || signedURL;
   }
   // add app to db
-  const { error: dbError } = await supabase
-    .from('apps')
-    .insert({
-      icon_url: signedURL,
-      user_id: userId,
-      name,
-      app_id: appId,
-    })
+  const { error: dbError } = await supabase.from("apps").insert({
+    icon_url: signedURL,
+    user_id: userId,
+    name,
+    app_id: appId,
+  });
   if (dbError) {
-    p.log.error(`Could not add app ${formatError(dbError)}`);
-    program.error('');
+    p.log.error(`dbError: Could not add app ${formatError(dbError)}`);
+    program.error(`user_id: ${userId} \n
+    app_id: ${appId}`);
   }
-  const { error: dbVersionError } = await supabase
-    .from('app_versions')
-    .insert([{
+  const { error: dbVersionError } = await supabase.from("app_versions").insert([
+    {
       user_id: userId,
       deleted: true,
-      name: 'unknown',
+      name: "unknown",
       app_id: appId,
-    }, {
-      user_id: userId,
-      deleted: true,
-      name: 'builtin',
-      app_id: appId,
-    }])
-  if (dbVersionError) {
-    p.log.error(`Could not add app ${formatError(dbVersionError)}`);
-    program.error('');
-  }
-  await snag.publish({
-    channel: 'app',
-    event: 'App Added',
-    icon: '🎉',
-    tags: {
-      'user-id': userId,
-      'app-id': appId,
     },
-    notify: false,
-  }).catch()
-  p.log.success(`App ${appId} added to Capgo. ${shouldExit ? 'You can upload a bundle now' : ''}`);
+    {
+      user_id: userId,
+      deleted: true,
+      name: "builtin",
+      app_id: appId,
+    },
+  ]);
+  if (dbVersionError) {
+    p.log.error(
+      `dbVersionError: Could not add app ${formatError(dbVersionError)}`
+    );
+    program.error(`user_id: ${userId} \n
+    app_id: ${appId}`);
+  }
+  await snag
+    .publish({
+      channel: "app",
+      event: "App Added",
+      icon: "🎉",
+      tags: {
+        "user-id": userId,
+        "app-id": appId,
+      },
+      notify: false,
+    })
+    .catch();
+  p.log.success(
+    `App ${appId} added to Capgo. ${
+      shouldExit ? "You can upload a bundle now" : ""
+    }`
+  );
   if (shouldExit) {
     p.outro(`Done ✅`);
-    process.exit()
+    process.exit();
   }
-  return true
-}
+  return true;
+};
 
 export const addCommand = async (apikey: string, options: Options) => {
-  addApp(apikey, options, true)
-}
+  addApp(apikey, options, true);
+};
